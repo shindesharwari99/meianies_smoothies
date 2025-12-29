59f4026cfc416f35105b8b25b148c5fd3307152a
shindesharwari99 <shindesharwari99@gmail.com>
2025-12-29 20:37:42 +0530

    Update streamlit_app.py
    added new section for website calling

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,3 +52,9 @@ if ingredients_list:
     # if ingredients_string:
     #     session.sql(my_insert_stmt).collect()
     #     st.success('Your Smoothie is ordered!', icon="✅")
+
+
+import requests
+smoothiefroot_response = requests.get("https://my.fruityvice.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
