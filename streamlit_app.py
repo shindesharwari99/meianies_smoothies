a7b229e9061d79315c22436f3714879aefcde9b8
shindesharwari99 <shindesharwari99@gmail.com>
2025-12-29 21:13:00 +0530

    Update streamlit_app.py
    added search_on column

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,14 +15,12 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write("The name on your Smoothie will be:", name_on_order)
 
-
-# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
 cnx = st.connection("snowflake")
 session = cnx.session()
 # session = get_active_session()
-my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
